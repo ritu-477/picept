5a3d41093d1d83637ced6987be36eea43885a2a1
ritu-477 <ritujoharbishnoi@gmail.com>
2024-11-19 16:54:29 +0530

    add ellipse

--- src/components/AiWithout.jsx
+++ src/components/AiWithout.jsx
@@ -4,10 +4,15 @@ import providerImg from '../assets/images/webp/provider-image.webp'
 import CommonHeading from '../common/CommonHeading'
 import PrimaryPara from '../common/PrimaryPara'
 import CommonButton from '../common/CommonButton'
+import aiEllipse from '../assets/images/webp/mission-ellipse.webp'
+import aiRightEllipse from '../assets/images/webp/ai-right-ellipse.webp'
+
 
 const AiWithout = () => {
   return (
-    <div className='lg:pt-[61px] md:pt-14 sm:pt-12 pt-6 md:pb-[68px] sm:pb-12 pb-6'>
+    <div className='lg:pt-[61px] md:pt-14 sm:pt-12 pt-6 md:pb-[68px] sm:pb-12 pb-6 relative'>
+      <img className='absolute left-0 -z-10 max-w-[411px] lg:block hidden h-[411px] top-[17%]' src={aiEllipse} alt="ai-ellipse" />
+      <img className='absolute right-0 -z-10 max-w-[411px] lg:block hidden h-[411px] top-[-20%]' src={aiRightEllipse} alt="ai-ellipse" />
       <div className='container'>
         <div className='lg:py-[66px] md:py-12 sm:py-8 py-6 px-6 rounded-[32px] bg-light-white'>
           <div className='flex lg:flex-row flex-wrap -mx-3 justify-between flex-col-reverse'>
